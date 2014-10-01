13c0d0ac5505816cddcb8984d06a7111ed3e571f
prufrock123 <shawnmllr86@gmail.com>
2014-09-30 19:58:15 -0500

    I think this is all done.

---
+++ main.js
@@ -0,0 +1,124 @@
+/**
+ * 1. create a new repo on Github,
+ * 2. add this script file to your index.html and run it in the browser to get the output
+ * 3. debug and see the console.log() messages in the chrome dev tools
+ */
+
+
+/**
+ * PART 1
+ *
+ * Write a JavaScript program that takes two numbers as parameters and computes the sum of those two numbers
+ *
+ * For example, if the user input 3 and 6, then the answer would be: "9 (3+6)".
+ */
+
+
+var summer = function(a, b) {
+	"use strict";
+	return a + b;
+}
+
+console.log(summer(8, 11));
+
+/**
+ * PART 2
+ *
+ * Write a JavaScript program that calculates the average time for this marathoner to complete the full marathon (26.2mi)
+ */
+
+
+var matt = {
+    finishingTime1: 240, //in minutes
+    finishingTime2: 210.4,
+    finishingTime3: 235.1,
+    finishingTime4: 208.9,
+    finishingTime5: 197.5
+};
+
+var mark = {
+    finishingTime1: 120, //in minutes
+    finishingTime2: 110.4,
+    finishingTime3: 135.1,
+    finishingTime4: 108.9,
+    finishingTime5: 97.5
+};
+
+// var getAverageTime = function(person) {
+// 	"use strict";
+// 	Object.keys(person).forEach(function (key) {
+// 		var val += person[key];
+// 	});
+// 	return val/5;
+// };
+
+var getAverageTime = function(person) {
+	"use strict";
+	var allTimes = Object.keys(person),
+		val = 0;
+	console.log(allTimes);
+	
+	for (var i=0; i < allTimes.length; i++) {
+		val += person[allTimes[i]];
+	};
+
+	var average = val/allTimes.length;
+
+	var niceAverage = Math.round(average * 100) / 100;
+	return niceAverage;
+	// 	var sumTimes += allTimes[i];
+	// };
+};
+
+console.log(getAverageTime(matt));
+console.log(getAverageTime(mark));
+
+// var allTimes = Object.keys(person);
+// console.log(allTimes);
+
+
+/**
+ * PART 3
+ *
+ *
+ * Modify the following to calculate the difference between the two accounts.
+ */
+
+/**
+ * declarations
+ */
+
+var MattsBankAccount = {
+    checking: 0,
+    savings: 0,
+    retirement: 0
+};
+
+var RobertosBankAccount = {
+    checking: 0,
+    savings: 0,
+    retirement: 0
+};
+
+function addToBank(account, savings, retirement, checking) {
+	"use strict";
+    account.savings = account.savings + savings;
+    account.retirement = account.retirement + retirement;
+    account.checking = account.checking + checking;
+}
+
+function getSumOfAccounts(account) {
+	"use strict";
+	return account.savings + account.retirement + account.checking
+
+    // return ...
+}
+
+addToBank(MattsBankAccount, 100, 10, 1);
+addToBank(RobertosBankAccount, 200, 50, 19);
+
+var sumOfMatts = getSumOfAccounts(MattsBankAccount);
+var sumOfRobertos = getSumOfAccounts(RobertosBankAccount);
+
+console.log(sumOfMatts - sumOfRobertos);
+
